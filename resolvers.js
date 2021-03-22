5d17956d0b43bcbdf1dd086c581df5eb020c5ac0
Joel Glovier <1319791+jglovier@users.noreply.github.com>
2021-03-22 10:41:17 -0700

    spike resolvers object

---
+++ resolvers.js
@@ -0,0 +1,18 @@
+{
+  Person: {
+    parents(person, args, context) {
+      return context.Person.findParents(person.id);
+    },
+    children(person, args, context) {
+      return context.Person.findChildren(person.id);
+    },
+  },
+  Query: {
+    getPeople(root, args, context) {
+      return context.person.findAll();
+    },
+    getPerson(root, args, context) {
+      return context.Person.findById(args.id);
+    },
+  },
+}
